84bafd7626502ba976cdc56975674ebb9ef546f9
Plēpis <ansis.plepis@if.lv>
2024-04-07 21:00:44 +0300

    Complete API communications

--- src/program.py
+++ src/program.py
@@ -1,11 +1,15 @@
 import os
-import requests
 from sys import argv
+import requests
+from openai import OpenAI
 
 options = {
-    "openai_api_key": "",
-    "openai_api_url": "https://api.openai.com/v1/chat/completions",
+    "openai_api_key": "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",
+    "showmediagrams_guidelines_url": "https://showme.redstarplugin.com/diagram-guidelines",
+    "showmediagrams_render_url": "https://showme.redstarplugin.com/render",
 }
+
+openai_client = OpenAI(api_key=options["openai_api_key"])
 
 
 def read_input_data():
@@ -34,54 +38,89 @@ def read_input_data():
     return input_data_lines
 
 
-def refine_prompt(initial_prompt):
-    diagram_prompt = "Can you write a representation of a context diagram for a system with the following requirements:"
-    refined_prompt = f"{diagram_prompt}\n{initial_prompt}"
-
-    print(f"[INFO] Successfully refined prompt:")
-    print(refined_prompt, end="\n\n")
-
-    return refined_prompt
-
-
-def create_diagram(input):
-    openai_api_key = options["openai_api_key"]
-
-    if openai_api_key is None:
-        raise ValueError("OpenAI API key is not set in environment variables.")
-
-    request_headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {openai_api_key}",
+def get_diagram_guidelines():
+    request_payload = {
+        "explicitlyRequestedByUserDiagramLanguage": "mermaid",
+        "diagramType": "graph",
     }
 
-    request_data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Hello!"},
-        ],
-    }
-
-    response = requests.post(
-        options["openai_api_url"], headers=request_headers, json=request_data
+    response = requests.get(
+        options["showmediagrams_guidelines_url"], params=request_payload
     )
 
-    if response.status_code == 200:
-        print("Response from OpenAI:", response.json())
-        print("\n")
-        print(response.json()["choices"][0]["message"]["content"])
-    else:
-        print("Error:", response.status_code, response.text)
-    # pass the preprocessed data and create a model
-    pass
+    if not response.ok:
+        error = response.text
+        raise RuntimeError(f"[ERROR] Unable to fetch diagram guidelines:\n{error}")
+
+    response_payload = response.json()
+    diagram_guidelines = response_payload["diagramGuidelines"]
+
+    return diagram_guidelines
+
+
+def refine_prompt(input_data):
+    system_message = get_diagram_guidelines()
+
+    # Preprocess input data here
+    user_message = input_data
+
+    print(f"[INFO] Successfully refined prompt:")
+    print(f"{system_message}\n{user_message}", end="\n\n")
+
+    return system_message, user_message
+
+
+def create_diagram(system_message, user_message):
+    # response = openai_client.chat.completions.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "system", "content": system_message},
+    #         {"role": "user", "content": user_message},
+    #     ],
+    # )
+
+    # response_content = response.choices[0].message.content
+
+    response_content = """
+```mermaid
+graph TB
+  U["User"] -- "Register" --> L["Login"]
+  U -- "Modify<br />Profile Details" --> UPD["Update Profile"]
+  U -- "Create<br />Sellable Products" --> CSP["Create Products"]
+  U -- "Purchase Products" --> PP["Purchase Products"]
+  U -- "Logout" --> LO[""]
+  A["Admin"] -- "Modify or Delete Products" --> CSP
+```
+"""
+
+    print("[INFO] Successfully created diagram:")
+    print(response_content, end="\n\n")
+
+    return response_content
 
 
 def render_diagram(diagram):
-    pass
+    request_payload = {
+        "diagramLanguage": "mermaid",
+        "diagramType": "graph",
+        "d2Theme": "neutral-grey_sketch",
+        "diagram": diagram,
+    }
+
+    response = requests.get(
+        options["showmediagrams_render_url"], params=request_payload
+    )
+
+    if not response.ok:
+        raise RuntimeError("[ERROR] Unable to fetch diagram guidelines")
+
+    response_payload = response.json()
+    print(response_payload["results"])
+
+    return response_payload
 
 
 input_data = read_input_data()
-refined_prompt = refine_prompt(input_data)
-diagram = create_diagram(refined_prompt)
+system_message, user_message = refine_prompt(input_data)
+diagram = create_diagram(system_message, user_message)
 output = render_diagram(diagram)
